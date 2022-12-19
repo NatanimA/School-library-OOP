b747bb49ede2c882c59a5d9c14bd1851d8577055
natanimabesha <natanimabesha@www.zhewutech.com>
2022-12-19 12:43:04 +0300

    fix linters

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_reader :id
-    attr_accessor :name , :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def initialize(age, name = 'unkown',parent_permission: true)
-        @id = Random.rand(1..99999)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'unkown', parent_permission: true)
+    @id = Random.rand(1..99_999)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def is_of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 
-    private :is_of_age?
+  private :of_age?
 
-    def can_use_services?
-        is_of_age? || @parent_permission ? true : false
-    end
+  def can_use_services?
+    of_age? || @parent_permission ? true : false
+  end
 end
