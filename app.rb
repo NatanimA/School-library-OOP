4f929de90d3f17aadf42c8f9f60a1c3bbbb43884
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-02 13:03:47 +0300

    Refactored Main.rb

--- app.rb
+++ app.rb
@@ -13,6 +13,43 @@ class App
     @rentals = []
   end
 
+  def interface_options
+  puts ''
+  puts " Please choose among the options below by pressing its corresponding number:
+            1. List all books
+            2. List all people
+            3. Create person account
+            4. Create a book
+            5. Create a rental
+            6. List all rentals for particular person
+            7. Exit"
+  gets.chomp
+end
+
+  def option
+  case interface_options
+  when '1'
+    list_all_books
+  when '2'
+    list_all_people
+  when '3'
+    create_person
+  when 
+    '4'
+    create_book
+  when '5'
+    create_rental
+  when '6'
+    list_all_rentals
+  when '7'
+    puts 'Thank you for using the app!'
+    return
+  else
+    puts 'Invalid input. Please try again'
+  end
+  option
+end
+
   def start_console
     option
   end
